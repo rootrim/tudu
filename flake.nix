1154d8ebe6f945bddffacf88638fe3fbf2fb187f
nixtrim <omuaz01@gmail.com>
2025-07-29 18:36:35 +0300

    added nix support and more

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "tudu: a terminal application for managing tasks without ease";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "tudu";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          buildInputs = with pkgs; [
+            pkg-config
+            openssl
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.pkg-config
+            pkgs.openssl
+          ];
+        };
+      }
+    );
+}
