1f196d4f4cef99686f41c53aadcc13b9996b8701
nixtrim <omuaz01@gmail.com>
2025-08-02 11:32:56 +0300

    added methods to task class

--- src/core/types.rs
+++ src/core/types.rs
@@ -3,16 +3,41 @@ use std::time::Duration;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Task {
-    id: u32,
-    name: String,
-    is_done: bool,
-    task_type: TaskType,
-    tags: Vec<String>,
+    pub id: u32,
+    pub name: String,
+    pub is_done: bool,
+    pub task_type: TaskType,
+    pub tags: Vec<String>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub enum TaskType {
     Basic,
     Counter(u32),
     Timed(#[serde(with = "humantime_serde")] Option<Duration>),
+}
+
+impl Task {
+    pub fn new(id: u32, name: String, task_type: TaskType) -> Self {
+        Self {
+            id,
+            name,
+            is_done: false,
+            task_type,
+            tags: Vec::new(),
+        }
+    }
+    pub fn check(&mut self) {
+        self.is_done = true;
+    }
+    pub fn add_tag(&mut self, tag: String) {
+        if !self.tags.contains(&tag) {
+            self.tags.push(tag);
+        }
+    }
+    pub fn remove_tag(&mut self, tag: String) {
+        if self.tags.contains(&tag) {
+            self.tags.retain(|t| t != &tag);
+        }
+    }
 }
