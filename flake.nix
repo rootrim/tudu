59eeccf9599f8bb3433f2cf15efa60d78313cc17
nixtrim <omuaz01@gmail.com>
2026-03-15 00:27:39 +0300

    flake formatting

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
-  description =
-    "Tudu: A terminal application for managing tasks with a reward mechanism.";
+  description = "Tudu: A terminal application for managing tasks with a reward mechanism.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
@@ -8,34 +7,36 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) { inherit system; };
+  outputs = {
+    self,
+    flake-utils,
+    naersk,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = (import nixpkgs) {inherit system;};
 
-        naersk' = pkgs.callPackage naersk { };
+      naersk' = pkgs.callPackage naersk {};
+    in {
+      defaultPackage = naersk'.buildPackage {
+        pname = "tudu";
+        version = "0.1.0";
+        src = ./.;
+      };
 
-      in {
-        defaultPackage = naersk'.buildPackage {
-          pname = "tudu";
-          version = "0.1.0";
-          src = ./.;
-        };
+      buildInputs = with pkgs; [openssl];
+      nativeBuildInputs = with pkgs; [pkg-config];
 
-        buildInputs = with pkgs; [ openssl ];
-        nativeBuildInputs = with pkgs; [ pkg-config ];
-
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            rustc
-            cargo
-            clippy
-            rust-analyzer
-            rustfmt
-            openssl
-            pkg-config
-          ];
-        };
-
-      });
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          rustc
+          cargo
+          clippy
+          rust-analyzer
+          rustfmt
+          openssl
+          pkg-config
+        ];
+      };
+    });
 }
